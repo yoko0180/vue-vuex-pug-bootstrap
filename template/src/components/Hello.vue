e7cdda4b13df99245f24d5124840bc55a4de3c63
yoko0180 <yoko0180@gmail.com>
2017-09-25 20:14:15 +0900

    stylus導入

--- template/src/components/Hello.vue
+++ template/src/components/Hello.vue
@@ -3,6 +3,14 @@
     h1 {{ msg }}
     button.btn.btn-default(@click.prevent="offsetPoint(1)") click me {{point}}
     h1 getter {{foobaa}}
+    <ul>
+      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
+      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+      <br>
+      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
+    </ul>
 </template>
 
 <script>
@@ -12,7 +20,7 @@ export default {
   name: 'hello',
   data() {
     return {
-      msg: 'Vue.js Vuex Pug Bootstrap テンプレート'
+      msg: 'Vue.js Vuex Pug Bootstrap Stylus テンプレート'
     }
   },
   computed: {
@@ -26,22 +34,15 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
+<style lang="stylus" scoped>
+ul
+  list-style-type: none
+  padding: 0
 
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
+li
   display: inline-block;
   margin: 0 10px;
-}
 
-a {
-  color: #42b983;
-}
+a
+  color: #42b983
 </style>
